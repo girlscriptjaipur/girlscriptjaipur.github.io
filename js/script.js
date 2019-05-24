3934ab0ab1273f53ca3a94e628aec18b9266cf26
hiteshsubnani0128 <hiteshsubnani734@gmail.com>
2019-05-24 16:16:03 +0530

    new button added

--- js/script.js
+++ js/script.js
@@ -1,4 +1,11 @@
 function showIt(){
     document.getElementById('hideIt').style.display = 'inline-block';
     document.getElementById('btnShow').style.display = 'none';
+    document.getElementById('btnShow1').style.display = 'inline-block';
+}
+
+function hideIt1(){
+    document.getElementById('hideIt').style.display = 'none';
+    document.getElementById('btnShow').style.display = 'inline-block';
+    document.getElementById('btnShow1').style.display = 'none';
 }
