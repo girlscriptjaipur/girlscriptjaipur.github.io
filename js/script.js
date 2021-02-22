41aec0b5397afe98387c84b28f0e1c2d344c1da5
Gunjan  Paul <gunjanpaul@Gunjans-MacBook-Pro.local>
2021-02-22 12:46:44 +0530

    CSS error resolved

--- js/script.js
+++ js/script.js
@@ -14,3 +14,15 @@ function readmore(n){
     document.getElementById('read'+n).style. display = 'inline-block';
     document.getElementById('rode'+n).style.display = 'none';
 }
+
+function loadMorePast(){
+    document.getElementById('hidePast').style.display = 'inline-block';
+    document.getElementById('loadMorePast').style.display = 'none';
+    document.getElementById('loadLessPast').style.display = 'inline-block';
+}
+
+function loadLessPast(){
+    document.getElementById('hidePast').style.display = 'none';
+    document.getElementById('btnShow').style.display = 'inline-block';
+    document.getElementById('loadLessPast').style.display = 'none';
+}
